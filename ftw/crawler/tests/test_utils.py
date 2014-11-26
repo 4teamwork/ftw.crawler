9aa99588951616fd705c90d669aa72a3e7ce274f
Lukas Graf <lukas.graf@4teamwork.ch>
2014-11-26 18:29:23 +0100

    Add ExtendedJSONEncoder that can serialize datetime objects.

--- ftw/crawler/tests/test_utils.py
+++ ftw/crawler/tests/test_utils.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+from ftw.crawler.utils import ExtendedJSONEncoder
 from ftw.crawler.utils import get_content_type
+from ftw.crawler.utils import isodatetime
 from unittest2 import TestCase
 
 
@@ -11,3 +14,26 @@ class TestGetContentType(TestCase):
     def test_also_works_for_mimetype_only(self):
         header_value = 'text/html'
         self.assertEquals('text/html', get_content_type(header_value))
+
+
+class TestISODateTime(TestCase):
+
+    def test_serializes_datetime_to_tz_aware_iso_8601(self):
+        dt = datetime(2014, 12, 31, 15, 45, 30, 999)
+        self.assertEquals('2014-12-31T15:45:30.000999Z',
+                          isodatetime(dt))
+
+
+class TestExtendedJSONEncoder(TestCase):
+
+    def test_serializes_datetime(self):
+        encoder = ExtendedJSONEncoder()
+        dt = datetime(2014, 12, 31, 15, 45, 30, 999)
+        self.assertEquals('["2014-12-31T15:45:30.000999Z"]',
+                          encoder.encode([dt]))
+
+    def test_delegates_unknown_objects_to_default_encoder(self):
+        encoder = ExtendedJSONEncoder()
+        data = [object()]
+        with self.assertRaises(TypeError):
+            encoder.encode(data)
--- ftw/crawler/utils.py
+++ ftw/crawler/utils.py
@@ -1,3 +1,15 @@
+import datetime
+import json
+
+
+def isodatetime(dt):
+    """Helper function to create a valid, time zone aware ISO 8601 datetime
+    in UTC (Zulu time).
+    """
+    fmt = "%Y-%m-%dT%H:%M:%S.%fZ"
+    return dt.strftime(fmt)
+
+
 def get_content_type(header_value):
     """Helps deal with the fact that the HTTP Content-Type header may also
     contain a charset declaration. For example:
@@ -8,3 +20,13 @@ def get_content_type(header_value):
     """
     if header_value is not None:
         return header_value.split(';')[0]
+
+
+class ExtendedJSONEncoder(json.JSONEncoder):
+    """JSONEncoder that can also serialize datetime objects.
+    """
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return isodatetime(obj)
+        else:
+            return super(ExtendedJSONEncoder, self).default(obj)
